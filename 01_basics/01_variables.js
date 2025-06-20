8142e04460c77bc7ed0b79324c4413ff25bfa6f5
SUBHAM2258 <2003subhamdash@gmail.com>
2025-06-20 06:12:17 +0000

    useb of let const var

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144662
+let accountEmail = "subham@google.com"
+var accountpassword = "12345"// not to use
+accountCity = "jaipur"// not to use
+let accountname;
+
+console.log(accountId);
+accountEmail = "abcd@gmail.com"
+accountpassword = "2121733140"
+accountCity = "bengaluru"
+
+console.table([accountCity,accountEmail,accountId,accountpassword,accountname]);
